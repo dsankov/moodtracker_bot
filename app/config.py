1935cee467e0ba3527b180dfa0a61ab922a52845
Dmitry Sankov <Dmitry.Sankov@gmail.com>
2025-03-05 14:04:38 +0300

    chore: Add FastAPI main app

--- app/config.py
+++ app/config.py
@@ -7,14 +7,21 @@ class Settings(BaseSettings):
     ADMIN_IDS: list[int]
     FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
     LOG_ROTATION: str = "10 MB"
-    
+    DB_URL: str = 'sqlite+aiosqlite:///data/db.sqlite3'
+        
+    BASE_URL: str =""
     
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), "..", ".env")
     )
     
+    @property
+    def hook_url(self) -> str:
+        return f"{self.BASE_URL}/webhook"
+    
+    
 settings = Settings()
 
-log_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "log.txt")
+log_file_path = os.path.join(os.path.dirname(__file__), "log.txt")
 logger.add(log_file_path, format=settings.FORMAT_LOG, level="INFO", rotation=settings.LOG_ROTATION)
 
--- app/main.py
+++ app/main.py
@@ -1,4 +1,27 @@
 from app.config import settings
 from loguru import logger
+from fastapi import FastAPI, Request
+from aiogram.types import Update
+from contextlib import asynccontextmanager
+import uvicorn
 
-logger.info("Приложение запущено")
+
+
+logger.info("Application started")
+
+# The @asynccontextmanager decorator is used to define an asynchronous context manager
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    logger.info("Lifespan started")
+    yield
+    logger.info("Lifespan ended")
+    
+app = FastAPI(lifespan=lifespan)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello, FastAPI!"}
+
+# Run Uvicorn only if the script is executed directly
+if __name__ == "__main__":
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
