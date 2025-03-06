a6f2a5906adcffd5139c618f59f7401a13d5562a
Dmitry Sankov <Dmitry.Sankov@gmail.com>
2025-03-06 14:32:56 +0300

    chore: Send start notification. Add aiogram-Dialogs. bug: Brake logging format

--- app/config.py
+++ app/config.py
@@ -1,15 +1,19 @@
 import os
+import sys
 from loguru import logger
+import logging
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
     BOT_TOKEN: str
     ADMIN_IDS: list[int]
+    INIT_DB: int
+    
     FORMAT_LOG: str = "{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}"
     LOG_ROTATION: str = "10 MB"
     DB_URL: str = 'sqlite+aiosqlite:///data/db.sqlite3'
         
-    BASE_URL: str =""
+    BASE_URL: str = ""
     
     model_config = SettingsConfigDict(
         env_file=os.path.join(os.path.dirname(__file__), "..", ".env")
@@ -22,29 +26,20 @@ class Settings(BaseSettings):
     
 settings = Settings()
 
+logger.remove()
+
 log_file_path = os.path.join(os.path.dirname(__file__), "log.txt")
+# logger.add(sys.stdout, format=settings.FORMAT_LOG, level="INFO", colorize=True)
 logger.add(log_file_path, format=settings.FORMAT_LOG, level="INFO", rotation=settings.LOG_ROTATION)
 
-log_config = {
-    "version": 1,
-    "disable_existing_loggers": False,
-    "formatters": {
-        "default": {
-            "()": "uvicorn.logging.DefaultFormatter",
-            "fmt": "%(asctime)s | %(levelname)s | %(message)s",
-            "datefmt": "%Y-%m-%d at %H:%M:%S",
-        },
-    },
-    "handlers": {
-        "default": {
-            "formatter": "default",
-            "class": "logging.StreamHandler",
-            "stream": "ext://sys.stdout",
-        },
-    },
-    "loggers": {
-        "uvicorn": {"handlers": ["default"], "level": "INFO"},
-        "uvicorn.access": {"handlers": ["default"], "level": "INFO"},
-    },
-}
+# class InterceptHandler(logging.Handler):
+#     def emit(self, record):
+#         level = record.levelname
+#         logger_opt = logger.opt(depth=6, exception=record.exc_info)
+#         logger_opt.log(level, record.getMessage())
+
+# # Step 4: Apply Loguru interceptor to Uvicorn loggers
+# uvicorn_loggers = ["uvicorn", "uvicorn.access", "uvicorn.error"]
+# for name in uvicorn_loggers:
+#     logging.getLogger(name).handlers = [InterceptHandler()]
 
--- app/main.py
+++ app/main.py
@@ -1,28 +1,35 @@
-from app.config import settings, log_config
-from loguru import logger
-from fastapi import FastAPI, Request
-from aiogram.types import Update
 from contextlib import asynccontextmanager
+
 import uvicorn
+from aiogram.types import Update
+from fastapi import FastAPI, Request
+from loguru import logger
 
-
+from app.bot import bot_factory
+from app.config import settings
 
 logger.info("Application started")
+
 
 # The @asynccontextmanager decorator is used to define an asynchronous context manager
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     logger.info(f"Lifespan for {app.title} started")
+    await bot_factory.start_bot()
     yield
+    await bot_factory.stop_bot()
     logger.info(f"Lifespan for {app.title} ended")
-    
+
+
 app = FastAPI(lifespan=lifespan)
+
 
 @app.get("/")
 async def root():
     logger.info(f"Processing root request")
     return {"message": "Hello, FastAPI!"}
 
+
 # Run Uvicorn only if the script is executed directly
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True, log_config=log_config)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
